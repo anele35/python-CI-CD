12fb7f55ed61dd9c099046668086683c7c21684d
anıl can <141331832+anele35@users.noreply.github.com>
2024-03-30 21:15:12 +0300

    Add files via upload

--- deneme.py.py
+++ deneme.py.py
@@ -4,7 +4,7 @@ isim=str(input())
 print("Merhaba" + " " + isim + ". Demek bu ay izin yaptık:) Hadi gel hesaplayalım!" )
 
 
-maas=int(input("Aylık net maaşını rakamlar ile girer misin?:"))
+maas=int(input("Aylık net maaşını rakamlar ile girer misin? Araya nokta koyma:"))
 izin=int(input("Bu ay kaç gün izin yaptığını rakamlar ile girer misin?:"))
 
 kalanmaas= float(maas - ((maas / 30) * izin))
